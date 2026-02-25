cab0912f5701e5fecf41807fff346f96bdaf8299
hemanth naripinni <hemanthnaripinni125@gmail.com>
2026-02-25 16:43:41 +0530

    0.0.1

--- build.sh
+++ build.sh
@@ -6,9 +6,8 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-if[[$CREATE_SUPERUSER]];
-then
-    python manage.py createsuperuser --no-input
+if [ "$CREATE_SUPERUSER" = "True" ]; then
+    python manage.py createsuperuser --no-input || true
 fi
 
 # python manage.py shell << END
