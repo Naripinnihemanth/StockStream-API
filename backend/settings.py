7d5c45163ca81863507609f59198e7f7d5285d76
hemanth naripinni <hemanthnaripinni125@gmail.com>
2026-02-25 18:01:15 +0530

    0.0.1

--- backend/settings.py
+++ backend/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_ROOT = "/media/"  
 MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
