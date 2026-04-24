bc1cf27b040e6252b490a1a5b2fa340292c634a2
hemanth naripinni <hemanthnaripinni125@gmail.com>
2026-04-24 16:08:37 +0530

    0.0.3

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,10 +1,10 @@
-# from django.contrib import admin
-# from .models import *
+from django.contrib import admin
+from .models import *
 
-# class addressAdmin(admin.ModelAdmin):
-#     list_display=("auther","city","default")
-# # admin.site.register(custemUserModel)
-# admin.site.register(historyModel)
-# admin.site.register(addreddModel,addressAdmin)
-# admin.site.register(UserOrdersModel)
-# # admin.site.register(AllOrderedProducts)
+class addressAdmin(admin.ModelAdmin):
+    list_display=("auther","city","default")
+# admin.site.register(custemUserModel)
+admin.site.register(historyModel)
+admin.site.register(addreddModel,addressAdmin)
+admin.site.register(UserOrdersModel)
+# admin.site.register(AllOrderedProducts)
